da76382f19b2a244803bfa28e9251c31a1ad3f4f
Yusuke Yagishita <yagisuke.0209@gmail.com>
2017-06-25 19:01:09 +0900

    new entry js

---
+++ ReduxSimpleStarter/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// create a new component. This component should produce
+// some HTML
+const App = () => {
+  return <div>hi!</div>;
+}
+
+// Take this component's generated HTML and put it
+// on the page (in the DOM)
+ReactDOM.render(
+  <App />, document.querySelector('.container')
+);
