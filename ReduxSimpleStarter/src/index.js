80abba79f4c87c1c6d56c9c9e1d3fe399705ce55
Yusuke Yagishita <yagisuke.0209@gmail.com>
2017-06-26 00:11:24 +0900

    add api key

--- ReduxSimpleStarter/src/index.js
+++ ReduxSimpleStarter/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const API_KEY = 'AIzaSyDJS5UK-ExFAPj2QFKCHtNUKFmUsZwWrgo';
+
 // create a new component. This component should produce
 // some HTML
 const App = () => {
