2007718f31fc8ff79778c28b8fa483821b75698b
Yusuke Yagishita <yagisuke.0209@gmail.com>
2017-06-26 00:45:59 +0900

    update term state

--- ReduxSimpleStarter/src/components/search_bar.js
+++ ReduxSimpleStarter/src/components/search_bar.js
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
-const SearchBar = () => {
-  return <input />;
+class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { term: ''};
+  }
+  render() {
+    return (
+      <div>
+        <input onChange={event => this.setState({ term: event.target.value})} />
+      </div>
+    );
+  }
 };
 
 export default SearchBar;
