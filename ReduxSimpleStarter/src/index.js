9e25dd44ae8c7d06676b28ae8aa90dcbb67ef20d
Yusuke Yagishita <yagisuke.0209@gmail.com>
2017-06-26 00:11:54 +0900

    create search bar component.

--- ReduxSimpleStarter/src/index.js
+++ ReduxSimpleStarter/src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
 const API_KEY = 'AIzaSyDJS5UK-ExFAPj2QFKCHtNUKFmUsZwWrgo';
 
 // create a new component. This component should produce
 // some HTML
 const App = () => {
-  return <div>hi!</div>;
-}
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
+};
 
 // Take this component's generated HTML and put it
 // on the page (in the DOM)
