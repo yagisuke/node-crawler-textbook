1e3d81e92bb70c5347e05f691392cc9dac734199
Yusuke Yagishita <yagisuke.0209@gmail.com>
2017-06-27 02:18:46 +0900

    display youtube search result items.

--- ReduxSimpleStarter/src/index.js
+++ ReduxSimpleStarter/src/index.js
@@ -2,17 +2,25 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 const API_KEY = 'AIzaSyDJS5UK-ExFAPj2QFKCHtNUKFmUsZwWrgo';
 
-YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
-  console.log(data);
-});
-
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({ key: API_KEY, term: 'UTADA HIKARU' }, (videos) => {
+      // this.setState({ videos: videos });
+      this.setState({ videos });
+    });
+  }
   render() {
     return (
       <div>
         <SearchBar />
+        <VideoList videos={ this.state.videos } />
       </div>
     );
   }
