4739af42669ee1ee6503d92224687b44e68baebd
Yusuke Yagishita <yagisuke.0209@gmail.com>
2017-06-27 01:35:48 +0900

    refactor index.js

--- ReduxSimpleStarter/src/index.js
+++ ReduxSimpleStarter/src/index.js
@@ -1,27 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
-
 import SearchBar from './components/search_bar';
-
 const API_KEY = 'AIzaSyDJS5UK-ExFAPj2QFKCHtNUKFmUsZwWrgo';
 
 YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
   console.log(data);
 });
 
-// create a new component. This component should produce
-// some HTML
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
-};
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
+}
 
-// Take this component's generated HTML and put it
-// on the page (in the DOM)
-ReactDOM.render(
-  <App />, document.querySelector('.container')
-);
+ReactDOM.render(<App />, document.querySelector('.container'));
