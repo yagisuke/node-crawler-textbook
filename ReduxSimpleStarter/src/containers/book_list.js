8a0de4a1185073ee572960abcc31579aa5a98a69
Yusuke Yagishita <yagisuke.0209@gmail.com>
2017-07-01 16:04:31 +0900

    show book list

--- ReduxSimpleStarter/src/containers/book_list.js
+++ ReduxSimpleStarter/src/containers/book_list.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-export default class BookList extends Component {
+class BookList extends Component {
   renderList() {
     return this.props.books.map((book) => {
       return (
-        <li key={book.title} className="list-group-item">
+        <li key={ book.title } className="list-group-item">
           { book.title }
         </li>
       );
@@ -19,3 +20,11 @@ export default class BookList extends Component {
     )
   }
 }
+
+function mapStateToProps(state) {
+  return {
+    books: state.books
+  };
+}
+
+export default connect(mapStateToProps)(BookList);
