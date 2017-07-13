4c90ca3b2c2a42b20bde0e67429deded1e081902
Yusuke Yagishita <yagisuke.0209@gmail.com>
2017-07-13 23:43:40 +0900

    delete br

--- ReduxSimpleStarter-master/src/index.js
+++ ReduxSimpleStarter-master/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { Router, browserHistory } from 'react-router';
-
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
