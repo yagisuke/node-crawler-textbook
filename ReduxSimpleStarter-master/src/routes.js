b2714e6400b1619205d4a5538f6910b7bcc6ae33
Yusuke Yagishita <yagisuke.0209@gmail.com>
2017-07-17 17:56:11 +0900

    nest component

--- ReduxSimpleStarter-master/src/routes.js
+++ ReduxSimpleStarter-master/src/routes.js
@@ -2,6 +2,13 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 import App from './components/app';
 
+const Greeting = () => {
+  return <div>hey there</div>
+}
+
 export default (
-  <Route path="/" component={ App } />
+  <Route path="/" component={ App }>
+    <Route path="greet" component={ Greeting } />
+  </Route>
+
 );
