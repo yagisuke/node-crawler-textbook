69a69c2106481616be446110ce1ec6d510271f14
Yusuke Yagishita <yagisuke.0209@gmail.com>
2017-07-18 23:08:43 +0900

    add index route component

--- ReduxSimpleStarter-master/src/routes.js
+++ ReduxSimpleStarter-master/src/routes.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 import App from './components/app';
+import PostsIndex from './components/posts_index';
 
 const Greeting = () => {
   return <div>hey there</div>
@@ -8,6 +9,7 @@ const Greeting = () => {
 
 export default (
   <Route path="/" component={ App }>
+    <IndexRoute component={ PostsIndex } />
     <Route path="greet" component={ Greeting } />
   </Route>
 
