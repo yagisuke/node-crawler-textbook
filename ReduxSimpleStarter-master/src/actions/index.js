3457e2b5fc11e2d581fbbf18cb174a10b8b9ee1d
Yusuke Yagishita <yagisuke.0209@gmail.com>
2017-07-26 00:54:37 +0900

    登録した情報をトップ画面で一覧表示

--- ReduxSimpleStarter-master/src/actions/index.js
+++ ReduxSimpleStarter-master/src/actions/index.js
@@ -4,7 +4,7 @@ export const FETCH_POSTS = 'FETCH_POSTS';
 export const CREATE_POSTS = 'CREATE_POSTS';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
-const API_KEY = '?key=lkajdsfapipwietpw';
+const API_KEY = '?key=yagisuke0209';
 
 export function fetchPosts() {
   const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
