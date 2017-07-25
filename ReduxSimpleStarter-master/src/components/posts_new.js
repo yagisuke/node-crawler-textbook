34814edb26bc60ba7053413daf3c304c0e1b5481
Yusuke Yagishita <yagisuke.0209@gmail.com>
2017-07-26 00:13:41 +0900

    create new post

--- ReduxSimpleStarter-master/src/components/posts_new.js
+++ ReduxSimpleStarter-master/src/components/posts_new.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import { reduxForm } from 'redux-form';
+import { createPost } from '../actions/index'
 
 class PostsNew extends Component {
   render() {
     const { fields: { title, categories, content }, handleSubmit } = this.props;
 
     return (
-      <form onSubmit={ handleSubmit }>
+      <form onSubmit={ handleSubmit(this.props.createPost) }>
         <h3>create a new posts</h3>
         <div className="form-group">
           <label>Title</label>
@@ -26,7 +27,9 @@ class PostsNew extends Component {
   }
 }
 
+
+
 export default reduxForm({
   form: 'PostsNewForm',
   fields: ['title', 'categories', 'content']
-})(PostsNew);
+}, null, { createPost })(PostsNew);
