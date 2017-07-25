87d462b78a0f010939cffcc331d6e084fd937ce6
Yusuke Yagishita <yagisuke.0209@gmail.com>
2017-07-26 01:51:37 +0900

    add loader on post show page

--- ReduxSimpleStarter-master/src/components/posts_show.js
+++ ReduxSimpleStarter-master/src/components/posts_show.js
@@ -1,9 +1,23 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPost } from '../actions/index';
 
 class PostsShow extends Component {
+  componentWillMount() {
+    this.props.fetchPost(this.props.params.id);
+  }
+
   render() {
+    if (!this.props.post) {
+      return <div>Loading...</div>;
+    };
+
     return <div>Show post { this.props.params.id }</div>;
   }
 }
 
-export default PostsShow;
+function mapStateToProps(state) {
+  return { post: state.posts.post };
+}
+
+export default connect(mapStateToProps, { fetchPost })(PostsShow);
