48c73e7e005447ae5cbacec1f4bbb0db31ddffcd
Yusuke Yagishita <yagisuke.0209@gmail.com>
2017-07-26 00:26:55 +0900

    add valid error style

--- ReduxSimpleStarter-master/src/components/posts_new.js
+++ ReduxSimpleStarter-master/src/components/posts_new.js
@@ -9,21 +9,21 @@ class PostsNew extends Component {
     return (
       <form onSubmit={ handleSubmit(this.props.createPost) }>
         <h3>create a new posts</h3>
-        <div className="form-group">
+        <div className={`form-group ${title.touched && title.invalid ? 'has-danger' : ''}`}>
           <label>Title</label>
           <input type="text" className="form-control" {...title} />
           <div className="text-help">
             { title.touched ? title.error : '' }
           </div>
         </div>
-        <div className="form-group">
+        <div className={`form-group ${categories.touched && categories.invalid ? 'has-danger' : ''}`}>
           <label>Categories</label>
           <input type="text" className="form-control" {...categories} />
           <div className="text-help">
             { categories.touched ? categories.error : '' }
           </div>
         </div>
-        <div className="form-group">
+        <div className={`form-group ${content.touched && content.invalid ? 'has-danger' : ''}`}>
           <label>Content</label>
           <textarea className="form-control" {...content} />
           <div className="text-help">
