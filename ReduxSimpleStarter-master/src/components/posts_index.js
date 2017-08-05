636939fefec2f0044a6182e61ea61fcf4e537ca7
Yusuke Yagishita <yagisuke.0209@gmail.com>
2017-08-05 17:02:49 +0900

    create post index page

---
+++ ReduxSimpleStarter-master/src/components/posts_index.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class PostsIndex extends Component {
+  render() {
+    return (
+      <div>
+        posts index
+      </div>
+    );
+  }
+}
+
+export default PostsIndex;
