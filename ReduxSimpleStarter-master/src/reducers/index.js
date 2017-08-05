7079ebd293bc53c841c3fa6b5050d04e3fa94176
Yusuke Yagishita <yagisuke.0209@gmail.com>
2017-08-05 23:26:18 +0900

    add text field

--- ReduxSimpleStarter-master/src/reducers/index.js
+++ ReduxSimpleStarter-master/src/reducers/index.js
@@ -1,5 +1,5 @@
 import { combineReducers } from 'redux';
-import { reducer as formReducer } from 'redux-from';
+import { reducer as formReducer } from 'redux-form';
 import PostsReducer from './reducer_posts';
 
 const rootReducer = combineReducers({
