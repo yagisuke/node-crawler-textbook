ed62244eb33d050c72a4ba26a10de593fcf75e53
Yusuke Yagishita <yagisuke.0209@gmail.com>
2017-08-06 15:06:40 +0900

    add callback on submit timing

--- ReduxSimpleStarter-master/src/components/posts_new.js
+++ ReduxSimpleStarter-master/src/components/posts_new.js
@@ -25,8 +25,9 @@ class PostsNew extends Component {
   }
 
   onSubmit(values) {
-    this.props.history.push('/');
-    this.props.createPost(values);
+    this.props.createPost(values, () => {
+      this.props.history.push('/');
+    });
   }
 
   render() {
