60d8f26f41bf19197342c4eabe32a8a646af8b42
Yusuke Yagishita <yagisuke.0209@gmail.com>
2017-08-06 15:10:25 +0900

    add posts show page

---
+++ ReduxSimpleStarter-master/src/components/posts_show.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class PostsShow extends Component {
+  render() {
+    return (
+      <div>
+        posts show!
+      </div>
+    );
+  };
+}
+
+export default PostsShow;
